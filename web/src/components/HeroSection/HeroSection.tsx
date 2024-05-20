2262f1775e68addec97a71d5aa0f872a2c42a2f6
Luiz2002 <quineche44@gmail.com>
2024-05-20 05:31:02 +0000

    update services section

--- web/src/components/HeroSection/HeroSection.tsx
+++ web/src/components/HeroSection/HeroSection.tsx
@@ -14,7 +14,7 @@ const HeroSection = () => {
             <div className="columnRight">
                 <h2>Nuestros Servicios</h2>
                 <p>
-                Ofrecemos una amplia gama de servicios, incluyendo soldadura, pintura de casas; construcción de estructuras metálicas; fabricación y mantenimiento de maquinarias de consumo; construcción de casas; y acabados de alta calidad.
+                Ofrecemos una amplia gama de servicios, incluyendo la fabricación de estructuras metálicas, soldadura, pintura, construcción (albañilería, enchapado, construcción de cimientos, ampliación y remodelación), diseño de proyectos eléctricos, mecánicos y manuales; además de la fabricación y mantenimiento de maquinarias de consumo, construcción de casas y acabados de alta calidad.
                 </p>
             </div>
         </S.StyledHeroSection>
