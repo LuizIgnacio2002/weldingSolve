0377026955bc44497e33af3735578870f660e6cc
Luiz2002 <quineche44@gmail.com>
2024-05-27 14:55:56 -0500

    update

--- web/src/components/Header/Header.tsx
+++ web/src/components/Header/Header.tsx
@@ -12,7 +12,7 @@ const Header = () => {
                     <li><a href="#hero-section">HOME</a></li>
                     <li><a href="#services-section">SERVICIOS</a></li>
                     <li><a href="#clients-section">CLIENTES</a></li>
-                    <li><a href="#about-us-section">NUESTROS SERVICIOS</a></li>
+                    <li><a href="#about-us-section">ACERCA DE NOSOTROS</a></li>
                     <li><a href="#contact-section">CONTÁCTANOS</a></li>
                 </ul>
             </nav>
