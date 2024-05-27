0b95139ef447ebf233b1247fede1d8d5ec44fda2
Luiz2002 <quineche44@gmail.com>
2024-05-27 14:10:26 -0500

    update

--- web/src/components/AboutUs/AboutUs.tsx
+++ web/src/components/AboutUs/AboutUs.tsx
@@ -8,7 +8,7 @@ const AboutUs = () => {
                 <h1>Acerca de nosotros</h1>
             </div>
             <p>
-            Con más de 12 años de trayectoria en el mercado nacional, nos dedicamos a proporcionar soluciones de servicio a medida y de alta calidad, ajustadas a tus necesidades particulares. Nuestro equipo, comprometido y especializado, va más allá de los servicios convencionales, adaptando y personalizando cada solución para satisfacer tus requerimientos específicos. Nuestra misión es brindar soluciones industriales excepcionales y personalizadas que excedan las expectativas de nuestros clientes. Aspiramos a ser el proveedor líder de servicios industriales y de construcción, reconocidos por nuestra innovación, calidad, precisión y dedicación a la satisfacción del cliente.
+            Con más de 12 años en el mercado nacional, ofrecemos soluciones de servicio a medida y alta calidad. Nuestro equipo especializado va más allá de los servicios convencionales, adaptando cada solución a tus necesidades. Nuestra misión es brindar soluciones industriales excepcionales y personalizadas que superen las expectativas de nuestros clientes. Aspiramos a ser líderes en servicios industriales y de construcción, reconocidos por nuestra innovación, calidad y dedicación a la satisfacción del cliente.
             </p>
         </div>
     </S.StyledAboutUsSection>
