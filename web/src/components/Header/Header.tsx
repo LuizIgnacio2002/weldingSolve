66ec8cd88f1ded115f2e50fad57db9a0db29bc4b
Luiz2002 <quineche44@gmail.com>
2024-05-27 11:58:05 -0500

    id reference linked

--- web/src/components/Header/Header.tsx
+++ web/src/components/Header/Header.tsx
@@ -2,21 +2,19 @@ import * as S from './HeaderStyles';
 import leftBlackContainer from '../../assets/leftBlackContainer.png';
 import logo from '../../assets/logo/logo.png';
 
-
 const Header = () => {
     return (
         <S.StyledHeader>
-            
             <div className="columnLeft">
                 <img src={logo} alt="logo" />
             </div>
             <nav>
                 <ul>
-                    <li><a href="/">Home</a></li>
-                    <li><a href="/about">Servicios</a></li>
-                    <li><a href="/about">Clientes</a></li>
-                    <li><a href="/about">Acerca de nosotros</a></li>
-                    <li><a href="/about">Contacto</a></li>
+                    <li><a href="#hero-section">Home</a></li>
+                    <li><a href="#services-section">Servicios</a></li>
+                    <li><a href="#clients-section">Clientes</a></li>
+                    <li><a href="#about-us-section">Acerca de nosotros</a></li>
+                    <li><a href="#contact-section">Contacto</a></li>
                 </ul>
             </nav>
         </S.StyledHeader>
