155b2cbab675d0240e5416e5a66faafe0dd3724d
Luiz Ignacio Quineche Casana <quineche44@gmail.com>
2025-08-11 04:09:38 +0000

    update logos

--- web/src/components/Clients/Clients.tsx
+++ web/src/components/Clients/Clients.tsx
@@ -9,6 +9,17 @@ import Disforvet from '../../assets/companiesColored/Disforvet.png';
 import BINSWANGER from '../../assets/companiesColored/BINSWANGER.png';
 import Alata from '../../assets/companiesColored/Alata.png';
 
+import startplus_sac from '../../assets/companiesColored/start_plus_logo.png'
+import tech from '../../assets/companiesColored/tech_logo.jpg'
+import rumi from '../../assets/companiesColored/rumi_import_logo.jpg'
+import gate_gourmet from '../../assets/companiesColored/gate_gourment_logo.png'
+import pil from '../../assets/companiesColored/pil_logo.webp'
+import minera_luisa from '../../assets/companiesColored/minera_luisa_logo.png'
+import comacsa from '../../assets/companiesColored/comacsa_logo.png'
+import holsin from '../../assets/companiesColored/holcim_logo.jpg'
+
+
+
 const Clients = () => {
     const settings = {
         dots: false,
@@ -30,20 +41,32 @@ const Clients = () => {
             </div>
             <Slider {...settings}>
                 <div>
-                    <img src={BASICO} alt="BASICO" />
+                    <img src={startplus_sac} alt="startplus_sac" />
                 </div>
                 <div>
-                    <img src={DestileriaAndina} alt="DestileriaAndina" />
+                    <img src={tech} alt="tech" />
                 </div>
                 <div>
-                    <img src={Disforvet} alt="Disforvet" />
+                    <img src={rumi} alt="rumi" />
                 </div>
                 <div>
-                    <img src={BINSWANGER} alt="BINSWANGER" />
+                    <img src={gate_gourmet} alt="gate_gourmet" />
                 </div>
                 <div>
-                    <img src={Alata} alt="Alata" />
+                    <img src={pil} alt="pil" />
                 </div>
+                                <div>
+                    <img src={minera_luisa} alt="minera_luisa" />
+                </div>
+                                <div>
+                    <img src={comacsa} alt="comacsa" />
+                </div>
+                <div>
+                    <img src={holsin} alt="holsin" />
+                </div>
+                
+
+
 
                 
             </Slider>
